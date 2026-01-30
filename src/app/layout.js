798ddbf7f08e68c206c1fd1c4d77424f77c91a2b
Rishabh Didwania <99093735+RDRishabh@users.noreply.github.com>
2026-01-30 17:50:26 +0530

    Updating GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,17 @@ export default function RootLayout({ children }) {
           href="https://api.fontshare.com"
           crossOrigin="anonymous"
         />
-
+        
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-W2KB3VDM');
+          `}
+        </Script>
+       
         {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
@@ -40,6 +50,15 @@ export default function RootLayout({ children }) {
 
       <body className="font-sans antialiased">
         {/* Client boundary starts here */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W2KB3VDM"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <BookCallProvider>
           <Navbar />
           <main>{children}</main>
