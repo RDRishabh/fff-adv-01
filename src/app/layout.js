7ee7b3e3fa22899b2cabab87a3188f081bebd179
Rishabh Didwania <99093735+RDRishabh@users.noreply.github.com>
2026-02-01 12:35:39 +0530

    Add Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,68 +1,77 @@
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import { BookCallProvider } from "../components/ui/BookCallContext";
+import { BookCallProvider } from "@/components/ui/BookCallContext";
 import Script from "next/script";
-
-// export const metadata = {
-//   title: "French Fry Features – Shopify Stores That Convert",
-//   description:
-//     "Launch a professionally built Shopify store designed to convert and scale.",
-// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Font Preconnect */}
         <link
           rel="preconnect"
           href="https://api.fontshare.com"
           crossOrigin="anonymous"
         />
-        
-        <Script id="gtm" strategy="afterInteractive">
+
+        {/* =========================
+            Google Tag Manager
+        ========================== */}
+        <Script id="gtm-base" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-W2KB3VDM');
           `}
         </Script>
-       
-        {/* Meta Pixel */}
+
+        {/* =========================
+            Meta (Facebook) Pixel
+        ========================== */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            {if(f.fbq)return;n=f.fbq=function(){
+            n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};
             if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
             n.queue=[];t=b.createElement(e);t.async=!0;
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}
-            (window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
+            (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', '1411291903775804');
             fbq('track', 'PageView');
           `}
         </Script>
-        
-        <Script id="gtm-pageview" strategy="afterInteractive">
+
+        {/* =========================
+            Google Analytics (GA4)
+        ========================== */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-46N195CXD2"
+          strategy="afterInteractive"
+        />
+
+        <Script id="ga4-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtmTrackPageView() {
-              window.dataLayer.push({ event: 'pageview', page: window.location.pathname });
-            }
-            gtmTrackPageView();
-            window.addEventListener('popstate', gtmTrackPageView);
-            window.addEventListener('pushstate', gtmTrackPageView);
-            window.addEventListener('replaceState', gtmTrackPageView);
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-46N195CXD2', {
+              page_path: window.location.pathname,
+            });
           `}
         </Script>
       </head>
 
       <body className="font-sans antialiased">
-        {/* Client boundary starts here */}
+        {/* =========================
+            GTM NoScript (BODY TOP)
+        ========================== */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-W2KB3VDM"
